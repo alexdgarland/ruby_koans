cbea71708bc44ddb307500eda07aa820aeda54e4
Alex Garland <alex.garland@shopify.com>
2022-02-24 21:35:28 +0000

    Complete about_dice_project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+
+  attr_reader :values
+
+  def initialize
+    @rng = Random.new
+  end
+
+  def roll(n)
+    @values = Array.new(n){ (@rng.rand * 6).ceil }
+  end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
@@ -48,6 +56,7 @@ class AboutDiceProject < Neo::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+    #   - Could inject the Random Number Generator used (defaulting to a "real" RNG but testing with one where we fully control the sequence)
   end
 
   def test_you_can_roll_different_numbers_of_dice
