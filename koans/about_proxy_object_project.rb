e399bd0eee1eee5eceb8309906b6a73d77366d3e
Alex Garland <alex.garland@shopify.com>
2022-02-25 09:49:22 +0000

    Complete about_proxy_object_project

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,12 +13,26 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
 
-  # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    @messages.append(method_name)
+    @object.send(method_name, *args, &block)
+  end
+
+  def called?(message_name)
+    @messages.include?(message_name)
+  end
+
+  def number_of_times_called(message_name)
+    @messages.count{|m| m == message_name}
+  end
+
 end
 
 # The proxy object should pass the following Koan:
